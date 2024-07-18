4c0dfa25f9bca752efec2f32fc8afb40dbe5e4c1
David Mereacre <d.mereacre@student.tue.nl>
2024-07-18 15:03:47 +0300

    Changed cell color values

--- src/grid/Cell.py
+++ src/grid/Cell.py
@@ -1,16 +1,19 @@
 from PyQt5.QtWidgets import QGraphicsRectItem
-from PyQt5.QtGui import QPainter, QColor, QBrush
+from PyQt5.QtGui import QPainter, QColor, QBrush, QPen
 from PyQt5.QtCore import QRectF, Qt
 
 class Cell(QGraphicsRectItem):
     stateColorMap = {
-        'empty': QColor(255, 255, 255),
+        'empty': QColor(245, 245, 245),
         'obstacle': QColor(102, 102, 102),
         'checked': QColor(198, 198, 198),
-        'start': QColor(0, 0, 255),
-        'end': QColor(255, 255, 0),
-        'path': QColor(255, 0, 255)
+        'start': QColor(65, 252, 3),
+        'end': QColor(252, 3, 3),
+        'path': QColor(152, 111, 191)
     }
+    
+    borderColor = QColor(102, 102, 102)
+    
     def __init__(self, x, y, size) -> None:
         super().__init__(0, 0, size, size)
         self.setPos(x * size, y * size)
@@ -18,8 +21,9 @@ class Cell(QGraphicsRectItem):
         self.updateColor()
 
     def updateColor(self) -> None:
-        color = self.stateColorMap.get(self.state, QColor(255, 255, 255))
+        color = self.stateColorMap.get(self.state, self.stateColorMap['empty'])
         self.setBrush(QBrush(color))
+        self.setPen(QPen(self.borderColor))
         
     def setState(self, state) -> None:
         self.state = state
