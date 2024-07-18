93e9ebecbfd1ec2aae51e0807043997be08934cf
David Mereacre <d.mereacre@student.tue.nl>
2024-07-18 17:54:27 +0300

    Added documentation to Cell.py and GridWidget.oy

--- src/grid/Cell.py
+++ src/grid/Cell.py
@@ -1,8 +1,27 @@
+"""
+Module under which the Cell class is defined.
+"""
+
 from PyQt5.QtWidgets import QGraphicsRectItem
-from PyQt5.QtGui import QPainter, QColor, QBrush, QPen
-from PyQt5.QtCore import QRectF, Qt
+from PyQt5.QtGui import QColor, QBrush, QPen
 
 class Cell(QGraphicsRectItem):
+    """
+    Class for defining a cell object in the grid.
+    
+    This class represents a cell object in the grid, which can be in one of the following states:
+        - empty: cell is empty;
+        - obstacle: cell is an obstacle, i.e., not traversable;
+        - checked: cell has been checked by a path finder;
+        - start: cell marks the source node for the path finder;
+        - end: cell marks the destination node for the path finder;
+        - path: cell is part of the path found by the path finder.
+
+    Args:
+        QGraphicsRectItem: The QGraphicsRectItem class provides a rectangle item that you can add to a QGraphicsScene.
+    """
+    
+    # Dictionary mapping state of cell to color in grid
     stateColorMap = {
         'empty': QColor(245, 245, 245),
         'obstacle': QColor(102, 102, 102),
@@ -12,6 +31,7 @@ class Cell(QGraphicsRectItem):
         'path': QColor(152, 111, 191)
     }
     
+    # Color of border of cell
     borderColor = QColor(102, 102, 102)
     
     def __init__(self, x, y, size) -> None:
@@ -21,13 +41,28 @@ class Cell(QGraphicsRectItem):
         self.updateColor()
 
     def updateColor(self) -> None:
+        """
+        Updates the color of the cell based on its state.
+        """
         color = self.stateColorMap.get(self.state, self.stateColorMap['empty'])
         self.setBrush(QBrush(color))
         self.setPen(QPen(self.borderColor))
         
     def setState(self, state) -> None:
+        """
+        Sets the state of the cell.
+
+        Args:
+            state (str): state of cell.
+        """
         self.state = state
         self.updateColor()
         
     def getState(self) -> str:
+        """
+        Returns the state of the cell.
+
+        Returns:
+            str: state of cell.
+        """
         return self.state
--- src/grid/GridWidget.py
+++ src/grid/GridWidget.py
@@ -1,11 +1,25 @@
+"""
+Module under which the GridWidget class is defined.
+
+"""
+
 from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView
 from PyQt5.QtCore import Qt, pyqtSlot
 from PyQt5.QtGui import QPainter
+
 from src.grid.Cell import Cell
 
 from src.eventHandlers.GridEventHandler import GridEventHandler
 
 class GridWidget(QGraphicsView):
+    """
+    Class for defining a grid widget.
+    
+    This class represents a grid widget, which is a QGraphicsView object that contains a grid of cell objects.
+
+    Args:
+        QGraphicsView: The QGraphicsView class provides a widget for displaying the contents of a QGraphicsScene.
+    """
     def __init__(self, rows, cols, cell_size) -> None:
         self.scene = QGraphicsScene()
         super().__init__(self.scene)
@@ -19,15 +33,17 @@ class GridWidget(QGraphicsView):
         self.setRenderHint(QPainter.Antialiasing)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        
         self.eventHandler = GridEventHandler(self)
         
         self.hasStartNode = False
         self.hasEndNode = False
     
-    '''
-    Initializes grid of cell objects
-    '''
+
     def initGrid(self) -> None:
+        """
+        Initializes the grid of cell objects.
+        """
         for row in range(self.rows):
             cell_row = []
             for col in range(self.cols):
@@ -38,22 +54,52 @@ class GridWidget(QGraphicsView):
         
     @pyqtSlot(int, int, str)
     def setCellState(self, row, col, state) -> None:
+        """
+        Sets the state of a cell in the grid.
+
+        Args:
+            row: row of cell.
+            col: col of cell.
+            state: state of cell.
+        """
         if 0 <= row < self.rows and 0 <= col < self.cols:
             self.cells[row][col].setState(state)
             
     def getStartNodeState(self) -> bool:
+        """
+        Returns the state of the start node:
+            - True if start node exists.
+            - False otherwise.
+        """
         return self.hasStartNode
             
     def updateStartNodeState(self) -> None:
+        """
+        Updates the state of the start node.
+        """
         self.hasStartNode = not self.hasStartNode
             
     def getEndNodeState(self) -> bool:
+        """
+        Returns the state of the end node:
+            - True if end node exists.
+            - False otherwise.
+        """
         return self.hasEndNode
     
     def updateEndNodeState(self) -> None:
+        """
+        Updates the state of the end node.
+        """
         self.hasEndNode = not self.hasEndNode 
         
     def resetGrid(self, option) -> None:
+        """
+        Resets the grid by calling a helper function based on the specified option.
+
+        Args:
+            option (str): option for resetting the grid.
+        """
         if option == 'all':
             self.resetAll()
         elif option == 'checked_path':
@@ -62,6 +108,9 @@ class GridWidget(QGraphicsView):
             self.resetObstacles()
                     
     def resetAll(self) -> None:
+        """
+        Resets all cells in the grid.
+        """
         for row in range(self.rows):
             for col in range(self.cols):
                 cell = self.cells[row][col]
@@ -70,6 +119,9 @@ class GridWidget(QGraphicsView):
                 self.hasEndNode = False
                 
     def resetCheckedPath(self) -> None:
+        """
+        Resets all checked and path cells in the grid.
+        """
         for row in range(self.rows):
             for col in range(self.cols):
                 cell = self.cells[row][col]
@@ -77,23 +129,32 @@ class GridWidget(QGraphicsView):
                     cell.setState('empty')
                     
     def resetObstacles(self) -> None:
+        """
+        Resets all obstacle cells in the grid.
+        """
         for row in range(self.rows):
             for col in range(self.cols):
                 cell = self.cells[row][col]
                 if cell.getState() == 'obstacle':
                     cell.setState('empty')
     
-    '''
-    Forwards mouse events to eventHandler
-    ''' 
     def mousePressEvent(self, event) -> None:
+        """
+        Forwards mouse press event to event handler.
+        """
         self.eventHandler.handleMousePress(event)
         super().mousePressEvent(event)
 
     def mouseMoveEvent(self, event) -> None:
+        """
+        Forwards mouse move event to event handler.
+        """
         self.eventHandler.handleMouseMove(event)
         super().mouseMoveEvent(event)
 
     def mouseReleaseEvent(self, event) -> None:
+        """
+        Forwards mouse release event to event handler.
+        """
         self.eventHandler.handleMouseRelease(event)
         super().mouseReleaseEvent(event)
